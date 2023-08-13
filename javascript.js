ca4278551281edcf37280c582918c72fe94e8768
if-phillip-break <Philtran2003@icloud.com>
2023-08-13 08:43:26 -0400

    Grid displays and hover feature works

--- javascript.js
+++ javascript.js
@@ -4,8 +4,13 @@ for (let i = 0; i < 16; i++){
     for (let j = 0; j < 16; j++){
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.borderColor = "black";
-        square.style.borderWidth = "thick";
         container.appendChild(square);
     }
 }
+
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "pink";
+    });
+});
